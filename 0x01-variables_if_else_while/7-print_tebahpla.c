bc0ee96be47fe6f6b66d1809c8e431af9323de04
ftigistu <ftigistu@gmail.com>
2022-03-12 00:18:37 -0800

    7th print alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char x;
+
+x = 'z';
+while (x >= 'a')
+{
+putchar(x);
+x++;
+}
+putchar('\n');
+}
