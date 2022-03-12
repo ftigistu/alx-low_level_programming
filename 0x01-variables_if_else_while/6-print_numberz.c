1affac6ea3764e221bc91044304a4ca4b3d7dbb0
ftigistu <ftigistu@gmail.com>
2022-03-12 00:10:39 -0800

    6th print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+
+x = 48;
+while (x > 58)
+{
+putchar(x);
+x++;
+}
+return (0);
+}
