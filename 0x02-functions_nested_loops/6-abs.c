8f77dd3e63c1cd66c3838ff743bb3e826a89485e
ftigistu <ftigistu@gmail.com>
2022-03-16 03:01:43 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - Checks if a character is lowercase.
+ * @n: The character to be checked.
+ *
+ * Return: 1 if character is lowercase, 0 otherwise.
+ */
+int _abs(int)
+{
+int x = abs(n);
+_putchar(x);
+return (0);
+}
